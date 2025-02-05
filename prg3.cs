f8fd690c92bea498909d9fbf17b260f4ab3a0a55
shrutijha2710 <shrutijha2216@gmail.com>
2025-02-05 12:33:08 +0530

    prg3.cs
    Write a program to show usage of this keyword

---
+++ prg3.cs
@@ -0,0 +1,22 @@
+using System;
+
+class Test
+{
+   int num;
+   Test(int num)
+   {
+       this.num=num;
+   }
+   public static void Main()
+   {
+       Test t=new Test(4);
+       Console.WriteLine("Value is"+ t.num);
+   }
+   }
+
+Output-
+Value is 4
+
+
+
+
